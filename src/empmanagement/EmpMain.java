6599524cdd7b61006070b4126a0c47863ae9587b
poix00 <148445330+poix00@users.noreply.github.com>
2024-06-10 17:52:30 +0900

    init
    추가

---
+++ src/empmanagement/EmpMain.java
@@ -0,0 +1,10 @@
+package empmanagement;
+
+import javax.swing.JPanel;
+
+public class EmpMain {
+	public static void main(String[] args) {
+		LoginUI frame = new LoginUI();
+		frame.setVisible(true);
+	}
+}
---
+++ src/empmanagement/LoginUI.java
@@ -0,0 +1,91 @@
+package empmanagement;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JPasswordField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class LoginUI extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField idtextField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginUI frame = new LoginUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LoginUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		idtextField = new JTextField();
+		idtextField.setBounds(86, 32, 319, 31);
+		contentPane.add(idtextField);
+		idtextField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("ID");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(46, 32, 40, 31);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblPassword = new JLabel("password");
+		lblPassword.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPassword.setBounds(12, 73, 74, 31);
+		contentPane.add(lblPassword);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(86, 73, 319, 31);
+		contentPane.add(passwordField);
+		
+		JButton btn1 = new JButton("회원가입");
+		btn1.setBounds(113, 136, 121, 36);
+		contentPane.add(btn1);
+		
+		JButton btn2 = new JButton("로그인");
+		btn2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				//검증 후  -> 
+				EmpManagementMainUI frame = new EmpManagementMainUI();
+				frame.setVisible(true);
+			}
+		});
+		
+		btn2.setBounds(284, 136, 121, 36);
+		contentPane.add(btn2);
+	}
+
+}
