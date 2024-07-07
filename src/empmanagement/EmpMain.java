41c44e1a0c7e48c53bdf3883cca7c1846c62ed56
poix00 <poix00@gmail.com>
2024-07-07 22:44:40 +0900

    asd
    dddddd

--- src/empmanagement/EmpMain.java
+++ src/empmanagement/EmpMain.java
@@ -5,6 +5,7 @@ import javax.swing.JPanel;
 public class EmpMain {
 	//branch test B
 	//dsfsdfdsfdsfs
+	//dsffffffffffffffffffff
 	public static void main(String[] args) {
 		LoginUI frame = new LoginUI();
 		frame.setVisible(true);
