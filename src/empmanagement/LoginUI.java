48dc12feb9ed3ffc1fc8220f2b01d7a44c9eca6e
poix00 <poix00@gmail.com>
2024-07-07 22:35:20 +0900

    test2
    rfgsdf

--- src/empmanagement/LoginUI.java
+++ src/empmanagement/LoginUI.java
@@ -18,6 +18,7 @@ import java.awt.event.MouseEvent;
 
 public class LoginUI extends JFrame {
 //branch tset A
+//분기점
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField idtextField;
